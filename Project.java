ebbed021f7fd62c2ce50f5250075dba81ed453cc
Shadowmeth <47725754+Shadowmeth@users.noreply.github.com>
2020-12-20 15:20:56 +0500

    Update Project.java

--- Project.java
+++ Project.java
@@ -10,6 +10,7 @@ public class Project {
 	
 	// extract the username from account line and return it
     private static String parseUserName(String account) {
+    	// saba:thedumbestdumbo#21516
         int colonIndex = account.indexOf(':');
         return account.substring(0, colonIndex);
     }
@@ -27,7 +28,7 @@ public class Project {
         String money = account.substring(hashIndex + 1);
         return Integer.parseInt(money);
     }
-	
+
     // get number of accounts
 	private static int getAccountCount() throws FileNotFoundException { 
 		File file = new File(fileName);
@@ -40,7 +41,7 @@ public class Project {
 		acc.close();
 		return count;
 	}
-	
+
 	private static String[] deleteArrayElement(String[] arr, String ele) {
 		String[] temp = new String[arr.length - 1];
 		for (int j = 0, i = 0; i < temp.length;) {
@@ -57,8 +58,13 @@ public class Project {
 		accounts = deleteArrayElement(accounts, account);
 		writeFile(accounts);
 	}
-	
-	// old contents are overwritten
+
+	private static void appendAccount(String account) throws IOException {
+		accounts = addArrayElement(accounts, account);
+		writeFile(accounts);
+	}
+
+	// old contents are overwritten by contents of array accounts
 	private static void writeFile(String[] accounts) throws IOException {
 		PrintWriter file = new PrintWriter(new BufferedWriter(new FileWriter(fileName)));
 		for (int i = 0; i < accounts.length; i++) {
@@ -66,7 +72,7 @@ public class Project {
 		}
 		file.close();
 	}
-		
+
 	// read contents of file in accounts
 	private static void readFile() throws FileNotFoundException {
 		accounts = new String[getAccountCount()];
@@ -78,17 +84,101 @@ public class Project {
 		}
 		acc.close();
 	}
-	
+
+	private static String getUserName() {
+		Scanner input = new Scanner(System.in);
+		String userName;
+		do {
+			System.out.print("Enter username: ");
+			userName = input.nextLine();
+			if (userName.length() < 3) {
+				System.out.println("Invalid Username entered.");
+			}
+		} while (userName.length() < 3);
+		return userName;
+	}
+
+	private static String getPassword() {
+		Scanner input = new Scanner(System.in);
+		String password;
+		do {
+			System.out.print("Enter password: ");
+			password = input.nextLine();
+			if (password.length() < 3) {
+				System.out.println("Invalid Password entered.");
+			}
+		} while (password.length() < 3);
+		return password;
+	}
+
+	private static int getMoney() {
+		Scanner input = new Scanner(System.in);
+		int money;
+		do {
+			System.out.print("Enter money: ");
+			money = input.nextInt();
+			if (money < 10000) {
+				System.out.println("Minimum 10k initial deposit is required.");
+			}
+		} while (money < 10000);
+		return money;
+	}
+
+	// returns true if the user exists in the file already
+	private static boolean userExists(String userName) {
+		for (int i = 0; i < accounts.length; i++) {
+			if (parseUserName(accounts[i]).equals(userName)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private static String constructAccount(String userName, String password, int money) {
+		return userName + ":" + password + "#" + String.valueOf(money);
+	}
+
+	private static String[] addArrayElement(String[] arr, String ele) {
+		String[] temp = new String[arr.length + 1];
+		int c = 0;
+		for (; c < arr.length; c++) {
+			temp[c] = arr[c];
+		}
+		temp[c] = ele;
+		return temp;
+	}
+
+	// takes inputs, create an account, updates the file
+	private static boolean createAccount() throws IOException {
+		String userName = getUserName();
+		if (userExists(userName)) { 
+			return false;
+		}
+
+		String password = getPassword();
+		int money = getMoney();
+		String account = constructAccount(userName, password, money);
+		appendAccount(account);
+
+		System.out.println("Your account has been created sucessfully");
+		return true;
+	}
+
 	public static void main(String[] args) {
-		
+
 		try {
-			
-			
+			readFile(); // VERY FIRST STEP FOR PROGRAM TO WORK
+			// We can do nothing if we don't have an array containing the file for processing
+			if (!createAccount()) {
+				System.out.println("Error creating account: Your account exists already");
+			}
+
+
 		} catch (Exception e) {
 			System.out.println("Error doing IO processing");
 			e.printStackTrace();
 		}
-		
+
 	}
 
 }
